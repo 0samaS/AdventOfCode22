4666d40345c573ac4edf3ab3dcf7fbdf7fdc8115
Osama Saeed <saeedosama78@gmail.com>
2022-12-03 09:52:44 -0800

    template

---
+++ day_template/sol.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int part1(std::ifstream& file)
+{
+}
+
+int part2(std::ifstream& file)
+{
+}
+
+int main()
+{
+    std::ifstream file ("test.txt");
+
+    // std::cout << part1(file) << std::endl;
+    // std::cout << part2(file) << std::endl;
+
+    return 0;
+}
